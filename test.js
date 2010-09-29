3cac2d6f1b2fe978d024e0e696c01cca79c6c7a7
Andreas Kalsch <andreaskalsch@gmx.de>
2010-09-29 17:03:19 +0200

    Small update for test.js

--- test.js
+++ test.js
@@ -11,6 +11,15 @@ var sys = require('sys'),
 
 
 
+// example form README.markdown
+
+// mySchema instanceof Schema === true
+var mySchema = Schema.create({type:'integer'});
+  
+// validation instanceof Schema.Validation === true
+var validation = mySchema.validate(5);
+
+
 
 var v;
 
